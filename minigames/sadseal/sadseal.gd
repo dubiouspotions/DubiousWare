4a15b33181eb9a7cd56475681798173f4b669747
ocwal <haggelinor@gmail.com>
2022-10-02 00:16:19 +0200

    Add animation and ending

--- minigames/sadseal/sadseal.gd
+++ minigames/sadseal/sadseal.gd
@@ -6,6 +6,8 @@ func getPlayerDidWin():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.play("pulse")
+	$monster.hide()
 	pass # Replace with function body.
 
 onready var _animated_sprite = $AnimatedSprite
@@ -13,5 +15,7 @@ onready var _animated_sprite = $AnimatedSprite
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta): 
 	if Input.is_action_pressed(self.player_index+"_action"):
-		print("eeeh")
 		_animated_sprite.play("cry")
+		$cry.hide()
+		$try.hide()
+		$monster.show()
