ce5b74fe37189f49aa9ad72e85383c4385308d9a
Nevyn Bengtsson <nevyn@alloverse.com>
2022-10-02 01:36:37 +0200

    endgame screen

--- gamestates/fullgame_win/fullgame_win.gd
+++ gamestates/fullgame_win/fullgame_win.gd
@@ -1,5 +1,6 @@
 extends Control
 
+var game
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,9 +9,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var winscore = -1
+	var winner
+	for p in game.players:
+		if p.score > winscore:
+			winner = p
+			winscore = p.score
+	$WhoWon.text = "Player " + winner.index + "\nWon!"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Button_pressed():
+	game.end()
--- shared/game.gd
+++ shared/game.gd
@@ -1,9 +1,11 @@
 extends Node2D
 class_name Game
 
+const TOTAL_MINIGAMES_IN_GAME = 10
 export var debug_level: String = ""
 var current_arcade
 var players = []
+var current_minigame_index = 0
 
 func _init():
 	self.name = "Game"
@@ -21,12 +23,24 @@ func start():
 	current_arcade = preload("res://gamestates/arcade/arcade.tscn").instance()
 	current_arcade.game = self
 	current_arcade.debug_level = self.debug_level
-	get_tree().get_root().add_child(current_arcade)
+	add_child(current_arcade)
+
+func finish_game():
+	var final_scene = preload("res://gamestates/fullgame_win/fullgame_win.tscn").instance()
+	final_scene.game = self
+	add_child(final_scene)
 	
 func play_next():
-	get_tree().get_root().remove_child(current_arcade)
+	remove_child(current_arcade)
 	current_arcade.queue_free()
 	current_arcade = null
+	current_minigame_index = current_minigame_index + 1
 	
-	self.debug_level = ""
-	start()
+	if current_minigame_index < TOTAL_MINIGAMES_IN_GAME:
+		start()
+	else:
+		finish_game()
+
+func end():
+	# go back to main menu
+	self.queue_free()
