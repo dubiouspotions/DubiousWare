24eb6b13415162974e49de24dbe941da8dbad2d0
Nevyn Bengtsson <nevyn@alloverse.com>
2022-10-01 23:02:28 +0200

    space: explosion when dying

--- minigames/spacejump/PlayerShip.gd
+++ minigames/spacejump/PlayerShip.gd
@@ -15,20 +15,27 @@ func _ready():
 
 func lose():
 	are_you_winning_son = false
-	self.visible = false
+	$explosion.emitting = true
+	$flame.visible = false
+	yield(get_tree().create_timer(0.2), "timeout")
+	$sprite.visible = false
+	yield(get_tree().create_timer(1.0), "timeout")
+	$explosion.emitting = false
+	acceleration = Vector2()
 	
 
 func _physics_process(delta):
-	var input = $"..".get_input_vector()
-	self.rotation = self.rotation + input.x * 5 * delta
-	
-	acceleration.y = input.y * 300
-	velocity = velocity + acceleration.rotated(self.rotation)*delta
-	move_and_slide(velocity, Vector2(0, -1))
-	
-	$flame.visible = input.y != 0
-	$flame.frame = randi() % 3
-	
-	if get_slide_count() > 0:
-		lose()
+	if are_you_winning_son:
+		var input = $"..".get_input_vector()
+		self.rotation = self.rotation + input.x * 5 * delta
+		
+		acceleration.y = input.y * 300
+		velocity = velocity + acceleration.rotated(self.rotation)*delta
+		move_and_slide(velocity, Vector2(0, -1))
+		
+		$flame.visible = input.y != 0
+		$flame.frame = randi() % 3
+		
+		if get_slide_count() > 0:
+			lose()
 	
