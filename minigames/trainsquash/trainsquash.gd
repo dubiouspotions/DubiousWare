ce8b094b67fbde6979bba16583dea2510f84c766
Patrik Sjöberg <voxxar@gmail.com>
2022-10-02 02:28:09 +0200

    Train animations!

--- minigames/trainsquash/trainsquash.gd
+++ minigames/trainsquash/trainsquash.gd
@@ -7,28 +7,114 @@ const train_exit = -1000
 # var a = 2
 # var b = "text"
 
-var gametime = 0
+var gametime = 0.0
+var anim: float = 0.0
+var started = false
+
+var doors_duration = 0.5
+var train_duration = 1.0
+var train_arrival_time: float = 0.0
+var train_departure_time: float = 9.0
+var train_entered = false
+var train_left = false
+
+var door_travel = 25
+
+var tween = Tween.new()
+var tween2 = Tween.new()
+
+
+func schedule(time, method):
+	var timer = Timer.new()
+	add_child(timer)
+	timer.process_mode = Timer.TIMER_PROCESS_PHYSICS
+	timer.one_shot = true
+	timer.wait_time = time
+	timer.connect("timeout", self, method)
+	timer.start()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_child(tween)
+	add_child(tween2)
 	gen_crowd()
-	$Train.position.x = train_enter
-	pass # Replace with function body.
+	
+	enter_train()
 
+	schedule(train_arrival_time + train_duration, "open_doors")
+	schedule(train_departure_time - doors_duration*2, "close_doors")
+	schedule(train_departure_time, "exit_train")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	gametime += delta
 	
-	var train_speed = 2000.0
-	if $Train.position.x > train_target:
-		$Train.position.x -= train_speed * delta
-	else:
-		$Train.position.x = train_target
-	
-	
 
+func enter_train():
+	var y = $Train.position.y
+	tween.interpolate_property(
+		$Train, "position", 
+		Vector2(train_enter, y), 
+		Vector2(train_target, y), 
+		train_duration,
+		Tween.TRANS_QUAD, Tween.EASE_OUT
+	)
+	tween.start()
 
+func exit_train():
+	var y = $Train.position.y
+	tween.interpolate_property(
+		$Train, "position", 
+		Vector2(train_target, y), 
+		Vector2(train_exit, y), 
+		train_duration,
+		Tween.TRANS_QUAD, Tween.EASE_IN
+	)
+	tween.start()
+	
+func open_doors():
+	var y = $Train/DoorLeft.position.y
+	tween.interpolate_property(
+		$Train/DoorLeft, "position", 
+		Vector2(0, y), 
+		Vector2(-door_travel, y), 
+		doors_duration,
+		Tween.TRANS_QUAD, Tween.EASE_IN_OUT
+	)
+	tween.start()
+	
+	y = $Train/DoorRight.position.y
+	tween2.interpolate_property(
+		$Train/DoorRight, "position", 
+		Vector2(0, y), 
+		Vector2(door_travel, y), 
+		doors_duration,
+		Tween.TRANS_QUAD, Tween.EASE_IN_OUT
+	)
+	tween2.start()
+	
+func close_doors():
+	var y = $Train/DoorLeft.position.y
+	tween.interpolate_property(
+		$Train/DoorLeft, "position", 
+		Vector2(-door_travel, y), 
+		Vector2(0, y), 
+		doors_duration,
+		Tween.TRANS_QUAD, Tween.EASE_IN_OUT
+	)
+	tween.start()
+	
+	y = $Train/DoorRight.position.y
+	tween2.interpolate_property(
+		$Train/DoorRight, "position", 
+		Vector2(door_travel, y), 
+		Vector2(0, y), 
+		doors_duration,
+		Tween.TRANS_QUAD, Tween.EASE_IN_OUT
+	)
+	tween2.start()
+	
 func gen_crowd():
 	var crowd = $Platform/GameArea/Commuters
 	var cs = load("res://minigames/trainsquash/Commuter.tscn")
