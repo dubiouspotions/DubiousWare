266bae164e55ec48ea9918b340678c684274f785
Jonny Strömberg <jonny.stromberg@gmail.com>
2022-10-02 10:07:01 +0200

    Add animations to SpaceShooter 🚀

--- minigames/spaceshooter/Player.gd
+++ minigames/spaceshooter/Player.gd
@@ -4,24 +4,35 @@ extends KinematicBody2D
 func _ready():
 	pass # Replace with function body.
 
-export (int) var speed = 20
+export (int) var speed = 130
 var Velocity = Vector2()
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed(self.get_parent().player_index+"_left"):
 		Velocity.x = -speed
+		$AnimatedSprite.play("left")
 	elif Input.is_action_pressed(self.get_parent().player_index+"_right"):
 		Velocity.x = speed
+		$AnimatedSprite.play("right")
 	else:
+		if $AnimatedSprite.animation == "right":
+			$AnimatedSprite.play("right", true)	
+		if $AnimatedSprite.animation == "left":
+			$AnimatedSprite.play("left", true)	
 		Velocity.x = 0
 	
 	if Input.is_action_pressed(self.get_parent().player_index+"_up"):
 		Velocity.y = -speed
+		$AnimatedSprite.play("forward")
 	elif Input.is_action_pressed(self.get_parent().player_index+"_down"):
 		Velocity.y = speed
+		$AnimatedSprite.play("backward")
 	else:
 		Velocity.y = 0
+		
+	if Velocity.x == 0 && Velocity.y == 0:
+		$AnimatedSprite.play("idle")
 		
 	if Input.is_action_pressed(self.get_parent().player_index+"_action"):
 		print(str(delta)+" lol "+self.get_parent().player_index)
