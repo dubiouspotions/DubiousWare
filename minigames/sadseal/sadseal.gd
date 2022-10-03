71494e7a6f1e001f37cf20926a54e6da105c5b16
Patrik Sjöberg <voxxar@gmail.com>
2022-10-03 21:17:27 +0200

    crying seal fails

--- minigames/sadseal/sadseal.gd
+++ minigames/sadseal/sadseal.gd
@@ -1,8 +1,10 @@
 extends BaseMiniGame
 
+var is_happy = true
 
 func getPlayerDidWin():
-	return true
+	return is_happy
+	
 func getInstruction():
 	return "try not to cry"
 # Called when the node enters the scene tree for the first time.
@@ -16,6 +18,7 @@ onready var _animated_sprite = $AnimatedSprite
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta): 
 	if Input.is_action_pressed(self.player_index+"_action"):
+		is_happy = false
 		_animated_sprite.play("cry")
 		$cry.hide()
 		$try.hide()
